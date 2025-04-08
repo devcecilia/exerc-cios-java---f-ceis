05f25cfb3645b9d1f8f0dd9ffcc3873530f07fc7
Cecília Silva <yunkso278@gmail.com>
2025-04-08 15:36:41 -0300

    Create MeuNome.java
    O código imprime o nome "Emilie" 10 vezes, numerando de 1 a 100. Ele usa um laço for para repetir a mensagem e exibe o número da repetição seguido do nome.

---
+++ MeuNome.java
@@ -0,0 +1,11 @@
+public class MeuNome02 {
+    public static void main(String[] args) {
+        String nome = "Emilie"; 
+        
+        for(int i = 1; i <= 10; i+=1) {
+            System.out.println(i + " : " + nome);
+        }
+    }
+    
+    
+}
